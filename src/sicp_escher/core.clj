2f070324084c3399a9682e47321e71a8072cca8d
Sergey Moshnikov <s.moshnikov@gmail.com>
2015-12-03 08:47:57 +0200

    Started to work on flip-vert

--- src/sicp_escher/core.clj
+++ src/sicp_escher/core.clj
@@ -1,2 +1,15 @@
-(ns sicp-escher.core)
+(ns sicp-escher.core
+  (:require [sicp-escher.vectors :as v]))
 
+(defn make-frame [origin edge1 edge2]
+  {:origin origin :e1 edge1 :e2 edge2})
+
+(defn map-to-frame [{origin :origin e1 :e1 e2 :e2} [x y]]
+  (v/add-vect
+    origin
+    (v/add-vect
+      (v/scale-vect e1 x)
+      (v/scale-vect e2 y))))
+
+(defn flip-vert [picture]
+  picture)
--- src/sicp_escher/main.clj
+++ src/sicp_escher/main.clj
@@ -1,26 +1,24 @@
 (ns sicp-escher.main
   (:require [quil.core :as q])
-  (:require [sicp-escher.vectors :as vect])
+  (:require [sicp-escher.core :as core])
   (:gen-class))
 
 (defn segments->picture [segments]
   (fn [frame]
     (doseq [[start end] segments]
       (q/line
-        (vect/map-to-frame frame start)
-        (vect/map-to-frame frame end)))))
+        (core/map-to-frame frame start)
+        (core/map-to-frame frame end)))))
 
 (def cross (segments->picture
              [[[0 0] [1.0 1.0]]
               [[0 1.0] [1.0 0]]
               [[0 0.2] [0.2 0]]]))
 
-(def picture cross)
+(def picture (core/flip-vert cross))
 
 (defn window-frame []
-  {:origin [0 0]
-   :e1     [(q/width) 0]
-   :e2     [0 (q/height)]})
+  (core/make-frame [0 0] [(q/width) 0] [0 (q/height)]))
 
 (defn draw []
   (picture (window-frame)))
