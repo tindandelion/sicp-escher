410791d33559cb33bc5335f2ac524cc659e827d0
Sergey Moshnikov <s.moshnikov@gmail.com>
2015-12-03 07:38:08 +0200

    Vectors turned into Clojure vectors

--- src/sicp_escher/vectors.clj
+++ src/sicp_escher/vectors.clj
@@ -1,5 +1,9 @@
 (ns sicp-escher.vectors)
 
-(defn add-vect [{x1 :x y1 :y} {x2 :x y2 :y}]
-  {:x (+ x1 x2) :y (+ y1 y2)})
+(defn add-vect [[x1 y1] [x2 y2]]
+  [(+ x1 x2) (+ y1 y2)])
+
+(defn scale-vect [[x y] factor]
+  [(* factor x) (* factor y)])
+
 
--- test/sicp_escher/vectors_test.clj
+++ test/sicp_escher/vectors_test.clj
@@ -2,20 +2,16 @@
   (:require [clojure.test :refer :all]
             [sicp-escher.vectors :refer :all]))
 
-
-(defn scale-vect [{x :x y :y} factor]
-  {:x (* factor x) :y (* factor y)})
-
 (testing "Vector arithmetic"
   (deftest add-vectors
-    (let [v1 {:x 1 :y 1}
-          v2 {:x 2 :y 3}]
-      (is (= {:x 3 :y 4} (add-vect v1 v2)))))
+    (let [v1 [1 1]
+          v2 [2 3]]
+      (is (= [3 4] (add-vect v1 v2)))))
 
   (deftest scale-vectors
-    (let [v {:x 2 :y 3}]
-      (is (= {:x 4 :y 6} (scale-vect v 2)))
-      (is (= {:x 1.0 :y 1.5} (scale-vect v 0.5)))
-      (is (= {:x (/ 4 3) :y 2} (scale-vect v (/ 2 3)))))))
+    (let [v [2 3]]
+      (is (= [4 6] (scale-vect v 2)))
+      (is (= [1.0 1.5] (scale-vect v 0.5)))
+      (is (= [(/ 4 3) 2] (scale-vect v (/ 2 3)))))))
 
 
