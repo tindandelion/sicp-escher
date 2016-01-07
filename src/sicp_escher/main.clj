7550cfaee65d559a11f1f29040b5071d698cbd23
Sergey Moshnikov <s.moshnikov@gmail.com>
2016-01-07 07:44:05 +0200

    Reoreganizing code to make place for SVG drawings

--- src/sicp_escher/main.clj
+++ src/sicp_escher/main.clj
@@ -2,18 +2,9 @@
   (:require [quil.core :as quil])
   (:require [sicp-escher.transforms :refer :all])
   (:require [sicp-escher.data :as data])
+  (:require [sicp-escher.data.lines :as lines])
   (:require [sicp-escher.canvas.quil :as canvas])
   (:gen-class))
-
-(defn compose-picture [t u]
-  (let [blank (fn [_])
-        side1 (quartet blank blank (rot-ccw t) t)
-        side2 (quartet side1 side1 (rot-ccw t) t)
-        corner1 (quartet blank blank blank u)
-        corner2 (quartet corner1 side1 (rot-ccw side1) u)
-        pseudocorner (quartet corner2 side2 (rot-ccw side2) (rot-ccw t))
-        entire-picture (cycled-quartet pseudocorner)]
-    entire-picture))
 
 (defn initial-transform [picture]
   (-> picture
@@ -30,7 +21,7 @@
   (quil/frame-rate 1)
   (quil/stroke-weight 1)
   (quil/background 200)
-  (quil/set-state! :picture (compose-picture data/t data/u))
+  (quil/set-state! :picture (data/compose-picture lines/t lines/u))
   (quil/no-loop))
 
 (defn -main []
