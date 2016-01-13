64cb98b382b2a152923439ec477fcad0bc0fb707
Sergey Moshnikov <s.moshnikov@gmail.com>
2016-01-13 08:27:51 +0200

    Code cleanup

--- test/sicp_escher/transforms_test.clj
+++ test/sicp_escher/transforms_test.clj
@@ -1,6 +1,6 @@
 (ns sicp-escher.transforms-test
   (:require [clojure.test :refer :all]
-            [sicp-escher.transforms :as core]
+            [sicp-escher.transforms :as tr]
             [sicp-escher.canvas.simple :as simple]))
 
 (def test-pic (fn [canvas] (:frame canvas)))
@@ -8,13 +8,13 @@
 (def canvas (simple/->SimpleCanvas frame))
 
 (deftest combining-picture-transformations
-  (let [combination (core/beside test-pic test-pic)]
+  (let [combination (tr/beside test-pic test-pic)]
     (is (= [{:origin [0.0 0.0] :e1 [50.0 0.0] :e2 [0.0 100.0]}
             {:origin [50.0 0.0] :e1 [50.0 0.0] :e2 [0.0 100.0]}]
            (combination canvas))
         "Placing pictures beside each other"))
 
-  (let [combination (core/below test-pic test-pic)
+  (let [combination (tr/below test-pic test-pic)
         upper {:origin [0.0 50.0] :e1 [100.0 0.0] :e2 [0.0 50.0]}
         lower {:origin [0.0 0.0] :e1 [100.0 0.0] :e2 [0.0 50.0]}]
     (is (= [upper lower] (combination canvas))
@@ -22,13 +22,13 @@
   )
 
 (deftest high-order-transformations
-  (let [flipped (core/flip-vert test-pic)]
+  (let [flipped (tr/flip-vert test-pic)]
     (is (= {:origin [0.0 100.0] :e1 [100.0 0.0] :e2 [0.0 -100.0]}
            (flipped canvas))
         "Flip a picture vertically"))
 
 
-  (let [combination (core/quartet test-pic test-pic test-pic test-pic)
+  (let [combination (tr/quartet test-pic test-pic test-pic test-pic)
 
         top-left {:origin [0.0 50.0] :e1 [50.0 0.0] :e2 [0.0 50.0]}
         top-right {:origin [50.0 50.0] :e1 [50.0 0.0] :e2 [0.0 50.0]}
@@ -40,7 +40,7 @@
         "Placing pictures in four quadrants"))
 
 
-  (let [combination (core/cycled-quartet test-pic)
+  (let [combination (tr/cycled-quartet test-pic)
         top-left {:origin [0.0 50.0] :e1 [50.0 0.0] :e2 [0.0 50.0]}
         top-right {:origin [50.0 100.0] :e1 [0.0 -50.0] :e2 [50.0 0.0]}
         btm-left {:origin [50.0 0.0] :e1 [0.0 50.0] :e2 [-50.0 0.0]}
